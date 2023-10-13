8671de49b1e055bf9dc369d49c0b5d4f1f5a44aa
Kingbyno <kingbyno007@gmail.com>
2023-10-13 00:26:43 +0000

    In and not out

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,8 @@
 -- SQL script that creates a table users following these 
 -- id, email, name country enumeration US,Co, Tn DEFAULT us
-
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN' ) DEFAULT 'US' NOT NULL
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        name VARCHAR(255),
+        country ENUM('US', 'CO', 'TN' ) DEFAULT 'US' NOT NULL   
 );
